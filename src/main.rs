c32044d3ce4404e924a02e44509572fc41bc8d14
Thomas Sheard <thomas.sheard26@gmail.com>
2025-08-16 00:57:23 +1000

    eulers numerical integration through simplified rk4 for single pendulum behaviour; something off with double pendulum equations

--- src/main.rs
+++ src/main.rs
@@ -26,13 +26,13 @@ fn to_cartesian(r: f32, theta: f32) -> Cartesian {
 struct Model {
 
     l1: f32,
-    theta1: f32,
+    theta_1: f32,
     m1: f32,
     v1: f32,
     a1: f32,
 
     l2: f32,
-    theta2: f32,
+    theta_2: f32,
     m2: f32,
     v2: f32,
     a2: f32,
@@ -49,15 +49,15 @@ fn model(_app: &App) -> Model {
         // all necessary values:
 
         l1: 100.0, // length (m)
-        theta1: 2.0, // angle (radians)
-        m1: 10.0, // mass (kg)
+        theta_1: 0.5, // angle (radians)
+        m1: 1.0, // mass (kg)
         v1: 0.0, // angular velocity (m/s)
         a1: 0.0, // angular acceleration (m/s/s)
 
         // and for second (child) pendulum
-        l2: 100.0,
-        theta2: -2.0,
-        m2: 10.0,
+        l2: 0.0,
+        theta_2: 0.0,
+        m2: 0.0,
         v2: 0.0,
         a2: 0.0,
 
@@ -70,18 +70,16 @@ fn model(_app: &App) -> Model {
 
 }
 
+fn ddot_thetas(model: &Model, theta_1: f32, theta_2: f32, dot_theta_1: f32, dot_theta_2: f32) -> (f32, f32) {
 
-// runs once a frame 
-fn update(app: &App, model: &mut Model, _update: Update) {
-
-    // cache reused values to reduce load
+    // cache reused values to reduce computation
 
     let total_mass = model.m1 + model.m2;
 
-    let sin_theta_1 = model.theta1.sin();
-    let sin_theta_2 = model.theta2.sin();
+    let sin_theta_1 = theta_1.sin();
+    let sin_theta_2 = theta_2.sin();
 
-    let dtheta = model.theta1 - model.theta2;
+    let dtheta = theta_1 - theta_2;
 
     let sin_dtheta = dtheta.sin();
     let cos_dtheta = dtheta.cos();
@@ -90,26 +88,39 @@ fn update(app: &App, model: &mut Model, _update: Update) {
 
     // p1 acceleration
 
-    model.a1 = -(sin_dtheta * (model.m2 * model.l1 * model.l1 * model.v1 * cos_dtheta + model.m2 * model.l2 * model.v1) - &model.gravity * (total_mass * sin_theta_1 - model.m2 * sin_theta_2 * cos_dtheta)) / (model.l1 * alpha);
+    let a1 = - model.gravity * sin_theta_1;
+        //-(sin_dtheta * (model.m2 * model.l1 * model.l1 * dot_theta_1 * cos_dtheta + model.m2 * model.l2 * dot_theta_1) - model.gravity * (total_mass * sin_theta_1 - model.m2 * sin_theta_2 * cos_dtheta)) / (model.l1 * alpha);
 
     // p2 acceleration
 
-    model.a2 = (sin_dtheta * (total_mass * model.l1 * model.v1 * model.v1 + model.m2 * model.l2 * model.v2 * model.v2 * cos_dtheta) + &model.gravity * (total_mass * sin_theta_1 * cos_dtheta - total_mass * sin_theta_2)) / (model.l2 * alpha);
+    let a2 = - model.gravity * sin_theta_2;
+        //(sin_dtheta * (total_mass * model.l1 * dot_theta_1 * dot_theta_1 + model.m2 * model.l2 * dot_theta_2 * dot_theta_2 * cos_dtheta) + model.gravity * (total_mass * sin_theta_1 * cos_dtheta - total_mass * sin_theta_2)) / (model.l2 * alpha);
 
-    let dt = app.duration.since_prev_update.as_secs_f32();
+    (a1, a2) // (a1, a2)
 
-    // currently a non-functional euler's method implementation
-    // this is bad as error propagates very quickly
-    // TODO: implement RK4 
+}
 
-    model.v1 += dt * model.a1;
-    model.v2 += dt * model.a2;
+// runs once a frame (defaults to 60?)
 
-    model.theta1 += dt * model.v1;
-    model.theta2 += dt * model.v2;
+fn update(app: &App, model: &mut Model, _update: Update) {
 
     // rk4 implementation
-    // ??
+     
+    let dt = app.duration.since_prev_update.as_secs_f32(); // accounts for variance in update rate
+
+    // current accelerations
+    let k1: (f32, f32) = ddot_thetas(model, model.theta_1, model.theta_2, model.v1, model.v2);
+    model.v1 += dt * k1.0;
+    model.v2 += dt * k1.1;
+
+    model.theta_1 += dt * model.v1;
+    model.theta_2 += dt * model.v2;
+        
+//    let k2: (f32, f32) = ddot_thetas(model,)
+//        // new projected velocity 1
+//        model.v1 + (dt / 2.0) * k1.0 ,
+//        //
+//    )
     
 
 }
@@ -120,8 +131,8 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     // cartesian tuples for the endpoint of each pendulum
     // stored relative to each pendulum's origin 
-    let p1 = to_cartesian(model.l1, model.theta1);
-    let p2 = to_cartesian(model.l2, model.theta2); 
+    let p1 = to_cartesian(model.l1, model.theta_1);
+    let p2 = to_cartesian(model.l2, model.theta_2); 
 
     draw.background().color(WHITESMOKE);
 
